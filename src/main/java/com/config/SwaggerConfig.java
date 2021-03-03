d2544c6e353b798e067de27c48d7a5294568dfee
huangrui <317894842@qq.com>
2021-03-03 18:01:11 +0800

    swagger

--- src/main/java/com/config/SwaggerConfig.java
+++ src/main/java/com/config/SwaggerConfig.java
@@ -2,7 +2,9 @@ package com.config;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -18,7 +20,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+@ComponentScan("com.controller")
+public class SwaggerConfig implements WebMvcConfigurer {
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
