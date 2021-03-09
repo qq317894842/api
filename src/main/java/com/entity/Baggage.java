7fa6e767ee06783adbf90b0e81f87e047012882b
huangrui <317894842@qq.com>
2021-03-09 17:39:54 +0800

    swagger

---
+++ src/main/java/com/entity/Baggage.java
@@ -0,0 +1,427 @@
+package com.entity;
+
+
+public class Baggage {
+
+  private long id;
+  private long flightId;
+  private String flightNo;
+  private java.sql.Date flightDate;
+  private String tagId;
+  private String sourceAirport;
+  private String destinationAirport;
+  private String specialBaggageType;
+  private String remark;
+  private long canLoad;
+  private long canTransfer;
+  private String abnormalCode;
+  private long prevId;
+  private String sourceId;
+  private String weightUnit;
+  private double checkinWeight;
+  private double carryonWeight;
+  private long checkinCount;
+  private String typeAndOutDes;
+  private String sizeUnit;
+  private double length;
+  private double width;
+  private double height;
+  private String pnrid;
+  private String uuid;
+  private String uuidType;
+  private String link;
+  private String organization;
+  private String passengerStatus;
+  private String passengerLevel;
+  private String passengerNo;
+  private String passengerFirstName;
+  private String passengerLastName;
+  private String passengerOtherName;
+  private String passengerSeat;
+  private String passengerBoardingNo;
+  private String passengerSecurityNo;
+  private String passengerCabin;
+  private double highLevelPassenger;
+  private double isSealed;
+  private String containerType;
+  private String seqId;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public long getFlightId() {
+    return flightId;
+  }
+
+  public void setFlightId(long flightId) {
+    this.flightId = flightId;
+  }
+
+
+  public String getFlightNo() {
+    return flightNo;
+  }
+
+  public void setFlightNo(String flightNo) {
+    this.flightNo = flightNo;
+  }
+
+
+  public java.sql.Date getFlightDate() {
+    return flightDate;
+  }
+
+  public void setFlightDate(java.sql.Date flightDate) {
+    this.flightDate = flightDate;
+  }
+
+
+  public String getTagId() {
+    return tagId;
+  }
+
+  public void setTagId(String tagId) {
+    this.tagId = tagId;
+  }
+
+
+  public String getSourceAirport() {
+    return sourceAirport;
+  }
+
+  public void setSourceAirport(String sourceAirport) {
+    this.sourceAirport = sourceAirport;
+  }
+
+
+  public String getDestinationAirport() {
+    return destinationAirport;
+  }
+
+  public void setDestinationAirport(String destinationAirport) {
+    this.destinationAirport = destinationAirport;
+  }
+
+
+  public String getSpecialBaggageType() {
+    return specialBaggageType;
+  }
+
+  public void setSpecialBaggageType(String specialBaggageType) {
+    this.specialBaggageType = specialBaggageType;
+  }
+
+
+  public String getRemark() {
+    return remark;
+  }
+
+  public void setRemark(String remark) {
+    this.remark = remark;
+  }
+
+
+  public long getCanLoad() {
+    return canLoad;
+  }
+
+  public void setCanLoad(long canLoad) {
+    this.canLoad = canLoad;
+  }
+
+
+  public long getCanTransfer() {
+    return canTransfer;
+  }
+
+  public void setCanTransfer(long canTransfer) {
+    this.canTransfer = canTransfer;
+  }
+
+
+  public String getAbnormalCode() {
+    return abnormalCode;
+  }
+
+  public void setAbnormalCode(String abnormalCode) {
+    this.abnormalCode = abnormalCode;
+  }
+
+
+  public long getPrevId() {
+    return prevId;
+  }
+
+  public void setPrevId(long prevId) {
+    this.prevId = prevId;
+  }
+
+
+  public String getSourceId() {
+    return sourceId;
+  }
+
+  public void setSourceId(String sourceId) {
+    this.sourceId = sourceId;
+  }
+
+
+  public String getWeightUnit() {
+    return weightUnit;
+  }
+
+  public void setWeightUnit(String weightUnit) {
+    this.weightUnit = weightUnit;
+  }
+
+
+  public double getCheckinWeight() {
+    return checkinWeight;
+  }
+
+  public void setCheckinWeight(double checkinWeight) {
+    this.checkinWeight = checkinWeight;
+  }
+
+
+  public double getCarryonWeight() {
+    return carryonWeight;
+  }
+
+  public void setCarryonWeight(double carryonWeight) {
+    this.carryonWeight = carryonWeight;
+  }
+
+
+  public long getCheckinCount() {
+    return checkinCount;
+  }
+
+  public void setCheckinCount(long checkinCount) {
+    this.checkinCount = checkinCount;
+  }
+
+
+  public String getTypeAndOutDes() {
+    return typeAndOutDes;
+  }
+
+  public void setTypeAndOutDes(String typeAndOutDes) {
+    this.typeAndOutDes = typeAndOutDes;
+  }
+
+
+  public String getSizeUnit() {
+    return sizeUnit;
+  }
+
+  public void setSizeUnit(String sizeUnit) {
+    this.sizeUnit = sizeUnit;
+  }
+
+
+  public double getLength() {
+    return length;
+  }
+
+  public void setLength(double length) {
+    this.length = length;
+  }
+
+
+  public double getWidth() {
+    return width;
+  }
+
+  public void setWidth(double width) {
+    this.width = width;
+  }
+
+
+  public double getHeight() {
+    return height;
+  }
+
+  public void setHeight(double height) {
+    this.height = height;
+  }
+
+
+  public String getPnrid() {
+    return pnrid;
+  }
+
+  public void setPnrid(String pnrid) {
+    this.pnrid = pnrid;
+  }
+
+
+  public String getUuid() {
+    return uuid;
+  }
+
+  public void setUuid(String uuid) {
+    this.uuid = uuid;
+  }
+
+
+  public String getUuidType() {
+    return uuidType;
+  }
+
+  public void setUuidType(String uuidType) {
+    this.uuidType = uuidType;
+  }
+
+
+  public String getLink() {
+    return link;
+  }
+
+  public void setLink(String link) {
+    this.link = link;
+  }
+
+
+  public String getOrganization() {
+    return organization;
+  }
+
+  public void setOrganization(String organization) {
+    this.organization = organization;
+  }
+
+
+  public String getPassengerStatus() {
+    return passengerStatus;
+  }
+
+  public void setPassengerStatus(String passengerStatus) {
+    this.passengerStatus = passengerStatus;
+  }
+
+
+  public String getPassengerLevel() {
+    return passengerLevel;
+  }
+
+  public void setPassengerLevel(String passengerLevel) {
+    this.passengerLevel = passengerLevel;
+  }
+
+
+  public String getPassengerNo() {
+    return passengerNo;
+  }
+
+  public void setPassengerNo(String passengerNo) {
+    this.passengerNo = passengerNo;
+  }
+
+
+  public String getPassengerFirstName() {
+    return passengerFirstName;
+  }
+
+  public void setPassengerFirstName(String passengerFirstName) {
+    this.passengerFirstName = passengerFirstName;
+  }
+
+
+  public String getPassengerLastName() {
+    return passengerLastName;
+  }
+
+  public void setPassengerLastName(String passengerLastName) {
+    this.passengerLastName = passengerLastName;
+  }
+
+
+  public String getPassengerOtherName() {
+    return passengerOtherName;
+  }
+
+  public void setPassengerOtherName(String passengerOtherName) {
+    this.passengerOtherName = passengerOtherName;
+  }
+
+
+  public String getPassengerSeat() {
+    return passengerSeat;
+  }
+
+  public void setPassengerSeat(String passengerSeat) {
+    this.passengerSeat = passengerSeat;
+  }
+
+
+  public String getPassengerBoardingNo() {
+    return passengerBoardingNo;
+  }
+
+  public void setPassengerBoardingNo(String passengerBoardingNo) {
+    this.passengerBoardingNo = passengerBoardingNo;
+  }
+
+
+  public String getPassengerSecurityNo() {
+    return passengerSecurityNo;
+  }
+
+  public void setPassengerSecurityNo(String passengerSecurityNo) {
+    this.passengerSecurityNo = passengerSecurityNo;
+  }
+
+
+  public String getPassengerCabin() {
+    return passengerCabin;
+  }
+
+  public void setPassengerCabin(String passengerCabin) {
+    this.passengerCabin = passengerCabin;
+  }
+
+
+  public double getHighLevelPassenger() {
+    return highLevelPassenger;
+  }
+
+  public void setHighLevelPassenger(double highLevelPassenger) {
+    this.highLevelPassenger = highLevelPassenger;
+  }
+
+
+  public double getIsSealed() {
+    return isSealed;
+  }
+
+  public void setIsSealed(double isSealed) {
+    this.isSealed = isSealed;
+  }
+
+
+  public String getContainerType() {
+    return containerType;
+  }
+
+  public void setContainerType(String containerType) {
+    this.containerType = containerType;
+  }
+
+
+  public String getSeqId() {
+    return seqId;
+  }
+
+  public void setSeqId(String seqId) {
+    this.seqId = seqId;
+  }
+
+}
